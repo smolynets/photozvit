c213d1f3846e80acccb4bdfb0eb525efa43d1ec7
Oleh Smolynets <smolynets@gmail.com>
2018-11-03 20:57:45 +0200

    fix: fix model and added Photo to admin

--- slider/models.py
+++ slider/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Photo(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=False, null=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Ptotos'
