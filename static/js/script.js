a8b8bfdd751af5f18bd79f29ce4c63ec596a4759
Oleh Smolynets <smolynets@gmail.com>
2018-11-03 22:11:14 +0200

    feat: auto for slider

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('.slider').bxSlider()
-});
+$(document).ready(function(e) {
+    $('.slider').bxSlider({
+      slideWidth: 812,
+      slideHeight:200,
+      minSlides: 1,
+      slideMargin: 0,
+      controls: false,
+      auto: true,
+      speed: 500
+    });
+  });
