d137d782c2319a792b55f8f8052075a5044911ba
Oleh Smolynets <smolynets@gmail.com>
2018-11-03 20:47:30 +0200

    feat: create Photo model and start view

--- slider/models.py
+++ slider/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(blank=False, null=False)
